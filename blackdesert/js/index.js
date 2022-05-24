4b6fb168f8e54ffe242c2d887dbcebb556044a24
wonpoem <hunstar20@naver.com>
2022-05-24 22:48:40 +0900

    05-24(2)

--- blackdesert/js/index.js
+++ blackdesert/js/index.js
@@ -13,5 +13,19 @@ $(function(){
     $(this).find(".more_submenu").stop().slideToggle(150)    
     });
 
+    $('.mobile_menu>a').click(function(){
+
+        $('.mobile_box').animate({left:'0'}, 500);
+        $('.menu_bg').show();
+
+        });
+        
+        $('.close').click(function(){
+        
+        $('.mobile_box').animate({left:'-363px'}, 500);
+        $('.menu_bg').hide();
+        
+        });
+
 
 });
